605ca4c0bc60d93181446b03763d9bfb0f905df1
Valery Juravlev <valera030219972010@gmail.com>
2020-01-01 22:15:50 +0300

    configuration

---
+++ src/assets/js/index.js
@@ -0,0 +1,8 @@
+// Main js file
+
+// const calendar = require('../../blocks/calendar/calendar.js');
+
+// $(document).ready(() => {
+
+// });
+
---
+++ src/index.js
@@ -0,0 +1,8 @@
+// JS
+import './assets/js/index.js'
+
+// SCSS
+import './main.scss'
+
+// CSS (example)
+// import './assets/css/main.css'
